ffaf45d529fe44249bf2fba6eff07e673cbff3c7
Lynda Sowunmi <lyndasownmi@yahoo.com>
2021-03-23 17:12:26 -0500

    Making the page prettier using CSS

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let partsOfSpeech = document.querySelector('.pos');
 let meaning = document.querySelector('.meaning');
 let offensives = document.querySelector('.offensives');
 let synonym = document.querySelector('.synonym');
-
+let sounds = document.querySelector('.sounds');
 
 
 button.addEventListener('click', function () {
@@ -18,12 +18,15 @@ button.addEventListener('click', function () {
       let meaningValue =  `Definition: `+ data[0].shortdef; // A short definition
       let offensivesValue = `Offensive word: `+ data[0]['meta'].offensive; //Checking if the word is offensive
       let synonymValue = `Synonyms: ` + data[0]['meta']['syns'][0]; // The synonyms
+      let soundsValue = data[0].hwi.prs[0].sound.audio;
       
        word.innerHTML = wordValue;
        partsOfSpeech.innerHTML = posValue;
        meaning.innerHTML = meaningValue;
        offensives.innerHTML = offensivesValue;
        synonym.innerHTML = synonymValue;
+       sounds.innerHTML = soundsValue;
+
 
     })
 
@@ -31,7 +34,6 @@ button.addEventListener('click', function () {
 })
 
 
-
-/*var msg = new SpeechSynthesisUtterance();
-msg.text = "My browser will say this sentence.";
-window.speechSynthesis.speak(msg); */
+let msg = new SpeechSynthesisUtterance();
+msg.text = "Call me Ray for short";
+window.speechSynthesis.speak(msg); 
