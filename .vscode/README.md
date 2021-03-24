bc761a84a9204afe9f514f33604cc82dc2b695e4
Lynda Sowunmi <lyndasownmi@yahoo.com>
2021-03-24 15:36:06 -0500

    Media queries styling

---
+++ .vscode/README.md
@@ -0,0 +1,11 @@
+Dictionary
+
+The source of the information used in JavaScript is an API key generated from Merriam-Webster's Thesaurus.
+
+This website was built to perform the basic functions of a dictionary/thesaurus. There are 5 items that will populate after text is inputted in the input field. They are:
+
+- The word itself
+- The part of speech
+- The definition
+- If the word is offensive or not
+- Synonyms list
--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let partsOfSpeech = document.querySelector('.pos');
 let meaning = document.querySelector('.meaning');
 let offensives = document.querySelector('.offensives');
 let synonym = document.querySelector('.synonym');
-let sounds = document.querySelector('.sounds');
 
 
 button.addEventListener('click', function () {
@@ -18,22 +17,22 @@ button.addEventListener('click', function () {
       let meaningValue =  `Definition: `+ data[0].shortdef; // A short definition
       let offensivesValue = `Offensive word: `+ data[0]['meta'].offensive; //Checking if the word is offensive
       let synonymValue = `Synonyms: ` + data[0]['meta']['syns'][0]; // The synonyms
-      let soundsValue = data[0].hwi.prs[0].sound.audio;
       
        word.innerHTML = wordValue;
        partsOfSpeech.innerHTML = posValue;
        meaning.innerHTML = meaningValue;
        offensives.innerHTML = offensivesValue;
        synonym.innerHTML = synonymValue;
-       sounds.innerHTML = soundsValue;
-
-
+      
     })
 
     .catch(err => alert("Wrong word or spelling!"));
 })
 
 
-let msg = new SpeechSynthesisUtterance();
+//data[0]['meta']['syns'][0]; // The synonyms
+ 
+
+/*let msg = new SpeechSynthesisUtterance(wordValue.value);
 msg.text = "Call me Ray for short";
-window.speechSynthesis.speak(msg); 
+window.speechSynthesis.speak(msg); */
